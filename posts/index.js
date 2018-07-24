8b4ab802d77b91bb99376bce7a28a9db0620180a
chenqipeng <chenqipeng.cqp@gmail.com>
2018-07-24 15:19:12 +0800

    文件读写

--- posts/index.js
+++ posts/index.js
@@ -1,7 +1,17 @@
 const fs = require('fs')
+const fm = require('front-matter')
+const _ = require('lodash')
 
-fs.readFile('archives/Array的forEach与splice.md', (err, logData) => {
+let attrStr = fs.readFileSync('attr.json', 'utf8')
+let attrObj = JSON.parse(attrStr)
+
+fs.readFile('archives/Array的forEach与splice.md', 'utf8', (err, logData) => {
   if (err) throw err;
-  let text = logData.toString();
-  console.log(text)
+  let text = fm(logData);
+  console.log(text.attributes)
+
+  let attr = text.attributes
+  
+  attrObj.posts = [attr]
+  fs.writeFileSync('attr.json', JSON.stringify(attrObj), 'utf8')
 });
